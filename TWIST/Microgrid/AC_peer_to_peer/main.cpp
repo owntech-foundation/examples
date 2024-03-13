6007de2a6e71127fb3466c47bdea081328a58dc1
rodgeur (Regis Ruelland) <regis.ruelland@laas.fr>
2024-03-13 17:38:15 +0100

    updated TWIST/AC_peer_to_peer example

--- TWIST/Microgrid/AC_peer_to_peer/main.cpp
+++ TWIST/Microgrid/AC_peer_to_peer/main.cpp
@@ -34,17 +34,22 @@
 #include "TaskAPI.h"
 #include "TwistAPI.h"
 #include "SpinAPI.h"
-#include "Rs485Communication.h"
-#include "SyncCommunication.h"
+#include "CommunicationAPI.h"
 
 // modules from control library
 #include "trigo.h"
 #include "pid.h"
 #include "pr.h"
-
+#include "ScopeMimicry.h"
 #include "zephyr/console/console.h"
 
 #define SERVER      // Role : SERVER or CLIENT 
+
+#ifdef SERVER
+#define ROLE_TXT "SERVER"
+#else
+#define ROLE_TXT "CLIENT"
+#endif
 
 //--------------SETUP FUNCTIONS DECLARATION-------------------
 void setup_routine(); // Setups the hardware and software of the system
@@ -71,22 +76,23 @@ static float32_t V_high;
 static float meas_data; // temp storage meas value (ctrl task)
 
 /* duty_cycle*/
-float32_t duty_cycle;
+static float32_t duty_cycle;
 
 /* Sinewave settings */
-const static float f0 = 50.F;
-const static float w0 = 2 * PI * f0;
-float angle = 0;
-
-/* PEER 2 PEER variables */
-float32_t V_ac;
-float32_t gain_current = -0.20;
-float32_t I_ac_ref;
-float32_t P_ref = 19; // 20W power reference from server to client
-float32_t v_dc_ref = sqrt(P_ref*115); // must be superior to at least 20V
-
-
+static const float f0 = 50.F;
+static const float w0 = 2 * PI * f0;
+#ifdef SERVER
+static float angle = 0;
+static float Vac_ref;
+#endif
+static const float Udc = 50.0;
 static const float32_t Ts = control_task_period * 1e-6F;
+/* PEER 2 PEER variables */
+static float32_t I_ac_ref;
+#ifdef CLIENT 
+static float32_t Vac_meas;
+static float32_t gain_current = -0.20;
+
 static const float32_t Kp = 0.01;
 static const float32_t Ti = 0.1;  // (Kp/Ki = Ti)
 static const float32_t Td = 0.0;
@@ -95,15 +101,21 @@ static const float32_t upper_bound = 2.0;
 static const float32_t lower_bound = -2.0;
 static Pid pid_current_control;
 static const PidParams pid_params(Ts, Kp, Ti, Td, N, lower_bound, upper_bound); 
+#endif
+static float32_t P_ref = 10; // 20W power reference from server to client
+static const float Rdc = 115;
+static float32_t v_dc_ref = sqrt(P_ref*Rdc); // must be superior to at least 20V
 
+
+static ScopeMimicry scope(1024, 9);
+static bool is_downloading = false;
 //------------- PR RESONANT -------------------------------------
-static const float32_t K_current = 60.0F;
-static const float32_t Kp_pr = 0.0005;
-static const float32_t Kr = 700.0;
-static float32_t pr_upper_bound = 30.0; // assume Udc ~ 30V
-static float32_t pr_lower_bound = -30.0;
+static const float32_t Kp_pr = 0.2;
+static const float32_t Kr = 3000.0;
+static float32_t pr_upper_bound = 50.0; // assume Udc ~ 50V
+static float32_t pr_lower_bound = -50.0;
 static Pr pr;
-static const PrParams pr_params(Ts, Kp_pr, Kr, 0.0, pr_lower_bound, pr_upper_bound);
+static const PrParams pr_params(Ts, Kp_pr, Kr, w0, 0.0, pr_lower_bound, pr_upper_bound);
 
 
 
@@ -122,20 +134,6 @@ extern float frequency;
 
 uint8_t status;
 uint32_t critical_task_counter;
-
-typedef struct Record
-{
-    float32_t I_low;
-    float32_t V_low;
-    float32_t Vhigh_value;
-    float32_t Iref;
-    float32_t duty_cycle;
-    float32_t Vref;
-    float32_t angle;
-} record_t;
-
-const uint16_t RECORD_SIZE = 2048;
-record_t record_array[RECORD_SIZE];
 uint32_t record_counter;
 
 //---------------------------------------------------------------
@@ -148,6 +146,30 @@ enum serial_interface_menu_mode // LIST OF POSSIBLE MODES FOR THE OWNTECH CONVER
 
 uint8_t mode = IDLEMODE;
 
+// trigger function for scope manager
+bool a_trigger() {
+    return true;
+}
+
+void dump_scope_data(ScopeMimicry &scope)  {
+    uint8_t *buffer = scope.get_buffer();
+    uint16_t buffer_size = scope.get_buffer_size() >> 2; // we divide by 4 (4 bytes per float data) 
+    printk("begin record\n");
+    printk("#");
+    for (uint16_t k=0;k < scope.get_nb_channel(); k++) {
+        printk("%s,", scope.get_channel_name(k));
+    }
+    printk("\n");
+    for (uint16_t k=0;k < buffer_size; k++) {
+        printk("%08x\n", *((uint32_t *)buffer + k));
+        task.suspendBackgroundUs(100);
+    }
+    printk("end record\n");
+}
+
+
+
+
 //--------------SETUP FUNCTIONS-------------------------------
 
 void reception_function(void)
@@ -159,7 +181,7 @@ void reception_function(void)
         status = rx_consigne.id_and_status;
 
         if ((rx_consigne.id_and_status & 2) == 1)
-            record_counter = 0;
+            scope.start(); 
 
         P_ref = rx_consigne.P_ref_fromSERVER;
     }
@@ -189,14 +211,14 @@ void setup_routine()
     twist.initLegBuck(LEG1);
     twist.initLegBoost(LEG2);
 
-    rs485Communication.configure(buffer_tx, buffer_rx, sizeof(consigne_struct), reception_function, 10625000, true); // custom configuration for RS485
+    communication.rs485.configure(buffer_tx, buffer_rx, sizeof(consigne_struct), reception_function, SPEED_20M); // custom configuration for RS485
 
 #ifdef SERVER
-    syncCommunication.initMaster(); // start the synchronisation
+    communication.sync.initMaster(); // start the synchronisation
 #endif
 
 #ifndef SERVER
-    syncCommunication.initSlave();
+    communication.sync.initSlave(TWIST_v_1_1_4);
 #endif
 
     // Then declare tasks
@@ -209,9 +231,26 @@ void setup_routine()
     task.startBackground(com_task_number);
     task.startCritical(); // Uncomment if you use the critical task
 
+#ifndef SERVER
     pid_current_control.init(pid_params);
     pid_current_control.reset(-gain_current); // output initialisation of the pid.
+#endif
     pr.init(pr_params);
+    scope.connectChannel(I1_low_value, "I1_low");
+    scope.connectChannel(I2_low_value, "I2_low");
+    scope.connectChannel(V1_low_value, "V1_low");
+    scope.connectChannel(V2_low_value, "V2_low");
+    scope.connectChannel(V_high, "V_high");
+    scope.connectChannel(I_high, "I_high");
+    scope.connectChannel(I_ac_ref, "Iac_ref");
+    scope.connectChannel(v_dc_ref, "Vdc_ref");
+    scope.connectChannel(duty_cycle, "duty_cycle");
+    scope.set_trigger(a_trigger);
+    scope.set_delay(0.0F);
+    scope.start();
+    I_ac_ref = 0.0;
+    v_dc_ref = 0.0;
+
 }
 
 //--------------LOOP FUNCTIONS--------------------------------
@@ -226,7 +265,7 @@ void loop_communication_task()
         case 'h':
             //----------SERIAL INTERFACE MENU-----------------------
             printk(" ________________________________________\n");
-            printk("|     ------- MENU ---------             |\n");
+            printk("      ------ MENU :%s             \n", ROLE_TXT);
             printk("|     press i : idle mode                |\n");
             printk("|     press p : power mode               |\n");
             printk("|________________________________________|\n\n");
@@ -235,11 +274,17 @@ void loop_communication_task()
         case 'i':
             printk("idle mode\n");
             mode = IDLEMODE;
+            printk("trig status :%d\n", scope.has_trigged());
+            scope.start();
+            scope.set_delay(0.0);
             record_counter = 0;
             break;
         case 'p':
             printk("power mode\n");
             mode = POWERMODE;
+            break;
+        case 'r':
+            is_downloading = true;
             break;
         default:
             break;
@@ -257,6 +302,12 @@ void loop_application_task()
     if (mode == IDLEMODE)
     {
         spin.led.turnOff();
+        if (is_downloading) {
+            dump_scope_data(scope);
+        }
+        is_downloading = false;
+
+
     }
     else if (mode == POWERMODE)
     {
@@ -265,11 +316,11 @@ void loop_application_task()
 #ifndef SERVER
         printk("%i:", status);
         printk("%f:", v_dc_ref);
-#endif
         printk("%f:", duty_cycle);
         printk("%f:", I2_low_value);
         printk("%f:", I1_low_value);
         printk("%f:\n", V1_low_value);
+#endif
     }
     task.suspendBackgroundMs(100);
 }
@@ -284,7 +335,7 @@ void loop_critical_task()
 {
     meas_data = data.getLatest(I1_LOW);
     if (meas_data < 10000 && meas_data > -10000)
-        I1_low_value = meas_data / 1000.0;
+        I1_low_value = meas_data;
 
     meas_data = data.getLatest(V1_LOW);
     if (meas_data < 10000 && meas_data > -10000)
@@ -296,7 +347,7 @@ void loop_critical_task()
 
     meas_data = data.getLatest(I2_LOW);
     if (meas_data < 10000 && meas_data > -10000)
-        I2_low_value = meas_data / 1000.0;
+        I2_low_value = meas_data;
 
     meas_data = data.getLatest(V_HIGH);
     if (meas_data != -10000)
@@ -305,7 +356,7 @@ void loop_critical_task()
 
     meas_data = data.getLatest(I_HIGH);
     if (meas_data < 10000 && meas_data > -10000)
-        I_high = meas_data/1000.0;
+        I_high = meas_data;
 
 #ifdef SERVER
 
@@ -315,7 +366,7 @@ void loop_critical_task()
         {
             twist.stopAll();
             tx_consigne.id_and_status = (1 << 6) + 0;
-            rs485Communication.startTransmission();
+            communication.rs485.startTransmission();
         }
         pwm_enable = false;
     }
@@ -325,7 +376,8 @@ void loop_critical_task()
 
         angle += w0 * Ts;
         angle = ot_modulo_2pi(angle);
-        duty_cycle = 0.5 + 0.15 * ot_sin(angle);
+        Vac_ref = 15.0F;
+        duty_cycle = 0.5 + Vac_ref * ot_sin(angle) / (2.0 * Udc);
         twist.setAllDutyCycle(duty_cycle);
 
         if (record_counter == 0)
@@ -335,26 +387,18 @@ void loop_critical_task()
 
         tx_consigne.P_ref_fromSERVER = P_ref;
 
-        rs485Communication.startTransmission();
+        communication.rs485.startTransmission();
 
-        if (critical_task_counter % 4 == 0)
+        if (critical_task_counter % 1 == 0)
         {
-            record_array[record_counter].I_low = I1_low_value;
-            record_array[record_counter].V_low = V1_low_value;
-            record_array[record_counter].Vhigh_value = V_high;
-            record_array[record_counter].duty_cycle = duty_cycle;
-            record_array[record_counter].Iref = 0;
-            record_array[record_counter].Vref = 0;
-            record_array[record_counter].angle = angle;
-            if (record_counter < (RECORD_SIZE-1))
-                record_counter++;
+            scope.acquire();
+            record_counter++;
         }
 
         if (!pwm_enable)
         {
             pwm_enable = true;
-            twist.startLeg(LEG1);
-            twist.startLeg(LEG2);
+            twist.startAll();
         }
 
 
@@ -369,11 +413,11 @@ void loop_critical_task()
     if ((((status & 0x3) == 1 || (status & 0x3) == 2)))
     {
         mode = POWERMODE;
-        v_dc_ref = sqrt(P_ref*115); // V_dc²/R = P, where R = 115Ω the output ressitor
-        V_ac = V1_low_value - V2_low_value;
-        gain_current = -pid_current_control.calculateWithReturn(v_dc_ref, V_high);
-        I_ac_ref = gain_current * V_ac;
-        duty_cycle = (V_ac +  pr.calculateWithReturn(I_ac_ref, I1_low_value)) / (2.0F * V_high) + 0.5F;
+        v_dc_ref = sqrt(P_ref*Rdc); // V_dc²/R = P, where R = 115Ω the output ressitor
+        Vac_meas = V1_low_value - V2_low_value;
+        gain_current = pid_current_control.calculateWithReturn(v_dc_ref, V_high);
+        I_ac_ref = -gain_current * Vac_meas;
+        duty_cycle = (Vac_meas + pr.calculateWithReturn(I_ac_ref, I1_low_value)) / (2.0F * Udc) + 0.5F;
         twist.setAllDutyCycle(duty_cycle);
 
         if (!pwm_enable)
@@ -385,15 +429,7 @@ void loop_critical_task()
 
         if (critical_task_counter % 4 == 0)
         {
-            record_array[record_counter].I_low = I1_low_value;
-            record_array[record_counter].V_low = V1_low_value;
-            record_array[record_counter].Vhigh_value = V_high;
-            record_array[record_counter].duty_cycle = duty_cycle;
-            record_array[record_counter].Iref = I_ac_ref;
-            record_array[record_counter].Vref = V_ac;
-            record_array[record_counter].angle = w0;
-            if (record_counter < (RECORD_SIZE-1))
-                record_counter++;
+            scope.acquire();
         }
         critical_task_counter++;
     }
