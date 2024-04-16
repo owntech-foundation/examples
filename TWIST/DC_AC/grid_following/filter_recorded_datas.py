eea7ec78e05226e549cf798cc1c4a17a1001e175
luizvilla <luizlavado@gmail.com>
2024-04-16 16:49:19 +0200

    Update filter_recorded_datas.py
    Added a copyright on the top

--- TWIST/DC_AC/grid_following/filter_recorded_datas.py
+++ TWIST/DC_AC/grid_following/filter_recorded_datas.py
@@ -1,3 +1,28 @@
+"""
+Copyright (c) 2021-2024 LAAS-CNRS
+
+  This program is free software: you can redistribute it and/or modify
+  it under the terms of the GNU Lesser General Public License as published by
+  the Free Software Foundation, either version 2.1 of the License, or
+  (at your option) any later version.
+
+  This program is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU Lesser General Public License for more details.
+
+  You should have received a copy of the GNU Lesser General Public License
+  along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+SPDX-License-Identifier: LGLPV2.1
+"""
+
+"""
+@brief  This is a class for the factory test of Twitst 1.4.1
+
+@author Regis Ruelland <rruelland@laas.fr>
+"""
+
 from platformio.public import DeviceMonitorFilterBase
 from datetime import datetime
 import struct
