72a3360657cdbb8ad128cd2226d1f2586627132d
Regis Ruelland <160136699+rrodgeur@users.noreply.github.com>
2024-06-12 15:29:21 +0200

    Update comments in filter_recorded_datas.py

--- TWIST/DC_DC/current_ripple_measurement/monitor/filter_recorded_datas.py
+++ TWIST/DC_DC/current_ripple_measurement/monitor/filter_recorded_datas.py
@@ -1,3 +1,26 @@
+##  Copyright (c) 2021-2024 LAAS-CNRS
+# 
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU Lesser General Public License as published by
+#    the Free Software Foundation, either version 2.1 of the License, or
+#    (at your option) any later version.
+# 
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU Lesser General Public License for more details.
+# 
+#    You should have received a copy of the GNU Lesser General Public License
+#    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# 
+#  SPDX-License-Identifier: LGPL-2.1
+
+## @brief This file is a python script that permits to write recorded data in an output txt file.
+# It defines and implements a custom platformio monitor filter. 
+# Get more info @ https://docs.platformio.org/en/latest/core/userguide/device/cmd_monitor.html#filters
+#
+# @author Régis Ruelland <regis.ruelland@laas.fr>
+
 from platformio.public import DeviceMonitorFilterBase
 from datetime import datetime
 import struct
