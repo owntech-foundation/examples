5d2b8a253459c3cea7d2e520e233d3c01012b5f4
Ayoub Farah <afarahhass@laas.fr>
2024-06-19 14:19:34 +0200

    Correct other files

--- SPIN/ADC/adc_hrtim_trigger/main.cpp
+++ SPIN/ADC/adc_hrtim_trigger/main.cpp
@@ -58,7 +58,7 @@ void setup_routine()
     // Setup the hardware first
     spin.version.setBoardVersion(SPIN_v_1_0);
 
-    spin.pwm.setFrequency(200000); // Set frequency of pwm
+    spin.pwm.initFrequency(200000); // Set frequency of pwm
 
     spin.pwm.setModulation(PWMA, UpDwn);
     spin.pwm.setAdcEdgeTrigger(PWMA, EdgeTrigger_up);
