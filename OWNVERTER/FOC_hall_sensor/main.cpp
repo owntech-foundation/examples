6a4c0b173e751791883df723e2223d3f5ba4c27f
Jean Alinei <jean.alinei@owntech.org>
2025-01-28 12:34:26 +0100

    FOC_hall_sensor - Comments fix
    Signed-off-by: Jean Alinei <jean.alinei@owntech.org>

--- OWNVERTER/FOC_hall_sensor/main.cpp
+++ OWNVERTER/FOC_hall_sensor/main.cpp
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021-2024 LAAS-CNRS
+ * Copyright (c) 2024-present LAAS-CNRS
  *
  *   This program is free software: you can redistribute it and/or modify
  *   it under the terms of the GNU Lesser General Public License as published by
@@ -26,8 +26,9 @@
  * @author Régis Ruelland <regis.ruelland@laas.fr>
  * @author Jean Alinei <jean.alinei@laas.fr>
  */
-//--------------OWNTECH APIs----------------------------------
-// #include "DataAPI.h"
+
+/* --------------OWNTECH APIs---------------------------------- */
+
 #include "ScopeMimicry.h"
 #include "SpinAPI.h"
 #include "TaskAPI.h"
@@ -38,22 +39,31 @@
 #include "trigo.h"
 #include "zephyr/console/console.h"
 
-//--------------SETUP FUNCTIONS DECLARATION-------------------
-void setup_routine(); // Setups the hardware and software of the system
+/* --------------SETUP FUNCTIONS DECLARATION------------------- */
 
-//--------------LOOP FUNCTIONS DECLARATION--------------------
-void loop_background_task(); // Code to be executed in the background task
-void loop_critical_task();   // Code to be executed in real time in the critical
+/* Setups the hardware and software of the system */
+void setup_routine();
+
+/* --------------LOOP FUNCTIONS DECLARATION-------------------- */
+
+/* Code to be executed in the background task */
+void loop_background_task();
+/* Code to be executed in real time in the critical */
+void loop_critical_task();
 void application_task();
-//--------------USER VARIABLES DECLARATIONS-------------------
+
+/* --------------USER VARIABLES DECLARATIONS------------------- */
 #define HALL1 PA7
 #define HALL2 PC6
 #define HALL3 PD2
 
 static const float32_t AC_CURRENT_LIMIT = 3.0;
 static const float32_t DC_CURRENT_LIMIT = 2.0;
-static const float32_t MIN_DC_VOLTAGE = 30.0F; // for control
-static const float32_t V_HIGH_MIN = 5.0;       // to start the POWER
+
+/* Used in the control algorithm */
+static const float32_t MIN_DC_VOLTAGE = 30.0F;
+/* Used as a threshold to start POWER mode */
+static const float32_t V_HIGH_MIN = 5.0;
 static const float32_t Ts = 100.e-6F;
 static const uint32_t control_task_period = (uint32_t)(Ts * 1.e6F);
 
@@ -68,14 +78,15 @@ static PllAngle pllangle = controlLibFactory.pllAngle(Ts, 10.0F, 0.04F);
 static PllDatas pllDatas;
 static float32_t angle_filtered;
 static float32_t w_meas;
+
 /*
- * one sector for one index value
- * index = H1*2^0 + H2*2^1 + H3*2^2
+ * One sector for one index value
+ * Index = H1*2^0 + H2*2^1 + H3*2^2
  */
 static int16_t sector[] = {-1, 5, 1, 0, 3, 4, 2};
 static float32_t k_angle_offset = 0.0F;
 
-/* LEG meas */
+/* Power LEG measures */
 static float32_t meas_data;
 static float32_t I1_low_value;
 static float32_t I2_low_value;
@@ -88,11 +99,11 @@ static float32_t V1_low_value;
 static float32_t V2_low_value;
 static float32_t V12_value;
 
-/* DC meas */
+/* DC measures */
 static float32_t I_high;
 static float32_t V_high;
 
-/* Three phase system and dqo */
+/* Three phase system and Park DQ Frame (dqo) */
 static three_phase_t Vabc;
 static three_phase_t duty_abc;
 static three_phase_t Iabc;
@@ -101,7 +112,7 @@ static dqo_t Idq;
 static dqo_t Idq_ref;
 static float32_t angle_4_control;
 
-/* variables used to get static value for scopemimicry */
+/* Variables used to get static value for ScopeMimicry */
 static three_phase_t Iabc_ref;
 static float32_t duty_a, duty_b;
 static float32_t Ia_ref;
@@ -116,7 +127,7 @@ static float32_t HALL2_value_f;
 static float32_t HALL3_value_f;
 static float32_t angle_index_f;
 
-// We make only torque control.
+/* We only make torque control. */
 static float32_t manual_Iq_ref;
 
 static LowPassFirstOrderFilter vHigh_filter = controlLibFactory.lowpassfilter(Ts, 5.0e-3F);
@@ -124,28 +135,39 @@ static LowPassFirstOrderFilter w_mes_filter = controlLibFactory.lowpassfilter(Ts
 static float32_t V_high_filtered;
 static float32_t inverse_Vhigh;
 
-/*--- PID ---*/
+/**
+ *  PID
+ */
+
 static float32_t Kp = 30 * 0.035;
 static float32_t Ti = 0.002029;
 static float32_t Td = 0.0F;
 static float32_t N = 1.0;
-/* coef 0.4 comes from Va_max =  (α_max - 0.5) * Udc */
+/* Coefficient 0.4 comes from Va_max =  (α_max - 0.5) * Udc     */
 static float32_t lower_bound = -MIN_DC_VOLTAGE * 0.4;
 static float32_t upper_bound = MIN_DC_VOLTAGE * 0.4;
-static Pid pi_d = controlLibFactory.pid(Ts, Kp, Ti, Td, N, lower_bound, upper_bound);
-static Pid pi_q = controlLibFactory.pid(Ts, Kp, Ti, Td, N, lower_bound, upper_bound);
+static Pid pi_d = controlLibFactory.pid(Ts, Kp, Ti, Td, N,
+										lower_bound, upper_bound);
+
+static Pid pi_q = controlLibFactory.pid(Ts, Kp, Ti, Td, N,
+										lower_bound, upper_bound);
+
+
+/* Decimation is used to limit the rate of measurement plotted in ScopeMimicry*/
 const static uint32_t decimation = 10;
 static uint32_t counter_time;
 float32_t counter_time_f;
 static float32_t w_estimate;
 uint8_t received_serial_char;
-enum serial_interface_menu_mode // LIST OF POSSIBLE MODES FOR THE OWNTECH
-				// CONVERTER
+
+/* List of possible modes for the OwnTech power shield */
+enum serial_interface_menu_mode
 {
 	IDLEMODE = 0,
 	POWERMODE = 1,
 };
 
+/* List of possible control states */
 enum control_state_mode {
 	OFFSET_ST = 0,
 	IDLE_ST = 1,
@@ -159,6 +181,11 @@ static float32_t control_state_f;
 static uint16_t error_counter;
 static bool pwm_enable;
 uint8_t asked_mode = IDLEMODE;
+
+/**
+ *  Definition of variables and functions for plotting real time values
+ *  using ScopeMimicry.
+ */
 
 const uint16_t SCOPE_SIZE = 512;
 uint16_t k_app_idx;
@@ -181,6 +208,9 @@ void dump_scope_datas(ScopeMimicry &scope) {
 	printk("end record\n");
 }
 
+/**
+ * VHigh Filter and PID init function
+ */
 void init_filt_and_reg(void)
 {
 	pllangle.reset(0.F);
@@ -191,7 +221,7 @@ void init_filt_and_reg(void)
 }
 
 /**
- * @brief a periodmeter function which estimate pulsation
+ * @brief A period-meter function which estimate pulsation
  * for the sector variable (one integer value correspond to π/3).
  *
  * @param sector assume sector is integer in [0, 5]
@@ -208,7 +238,7 @@ float32_t pulsation_estimator(int16_t sector, float32_t time)
 	delta_sector = sector - prev_sector;
 	prev_sector = sector;
 	if (delta_sector == 1 || delta_sector == -5) {
-		// positive speed
+		/* positive speed */
 		sixty_degre_step_time = (time - prev_time);
 		w_estimate_intern = (PI / 3.0) / sixty_degre_step_time;
 		prev_time = time;
@@ -221,6 +251,9 @@ float32_t pulsation_estimator(int16_t sector, float32_t time)
 	return w_estimate_intern;
 }
 
+/**
+ *  Function that retrieves necessary measurements from power shield sensors.
+ */
 inline void retrieve_analog_datas()
 {
 	meas_data = shield.sensors.getLatestValue(I1_LOW);
@@ -245,7 +278,8 @@ inline void retrieve_analog_datas()
 
 	meas_data = shield.sensors.getLatestValue(I_HIGH);
 	if (meas_data != NO_VALUE) {
-		I_high = -meas_data; // normal when we see the kicad
+		/* Sign is negative because of the way hardware sensor is routed */
+		I_high = -meas_data;
 	}
 
 	meas_data = shield.sensors.getLatestValue(V1_LOW);
@@ -258,20 +292,28 @@ inline void retrieve_analog_datas()
 		V2_low_value = meas_data;
 	}
 
-	// Mesure de Vhigh
+	/* Vhigh measurement gets additional filtering */
 	V_high_filtered = vHigh_filter.calculateWithReturn(V_high);
 
 	V12_value = V1_low_value - V2_low_value;
 }
 
+/**
+ * Reads Hall sensors and estimate position and speed.
+ */
 inline void get_position_and_speed()
 {
-	// we build angle index using HALL values.
+	/* We get individual HALL sensor readings */
 	HALL1_value = spin.gpio.readPin(HALL1);
 	HALL2_value = spin.gpio.readPin(HALL2);
 	HALL3_value = spin.gpio.readPin(HALL3);
+	/* We compute angle index using HALL values. */
 	angle_index = HALL3_value * 4 + HALL2_value * 2 + HALL1_value * 1;
-	hall_angle = ot_modulo_2pi(PI / 3.0 * sector[angle_index] + PI * k_angle_offset / 24.0);
+
+	hall_angle =
+			ot_modulo_2pi(PI / 3.0 * sector[angle_index] +
+			PI * k_angle_offset / 24.0);
+
 	w_estimate = pulsation_estimator(sector[angle_index], counter_time * Ts);
 	pllDatas = pllangle.calculateWithReturn(hall_angle);
 
@@ -279,6 +321,9 @@ inline void get_position_and_speed()
 	w_meas = w_mes_filter.calculateWithReturn(pllDatas.w);
 }
 
+/**
+ * Handle current limits and switch to Error state if limits exceeded.
+ */
 inline void overcurrent_mngt()
 {
 	if (I1_low_value > AC_CURRENT_LIMIT || I1_low_value < -AC_CURRENT_LIMIT ||
@@ -291,37 +336,52 @@ inline void overcurrent_mngt()
 	}
 }
 
+/**
+ * Stop PWM and reset filter and PID states
+ */
 inline void stop_pwm_and_reset_states_ifnot()
 {
 	if (pwm_enable == true) {
 		shield.power.stop(ALL);
-		// reset filters and pid
+		/* Reset filters and pid */
 		init_filt_and_reg();
 		pwm_enable = false;
 	}
 }
 
+/**
+ * Perform Torque control using Field Oriented Control algorithm
+ */
 inline void control_torque()
 {
+
 	angle_4_control = angle_filtered;
 	Idq_ref.q = manual_Iq_ref;
+
+	/* Saturation */
 	if (Idq_ref.q > Iq_max) {
 		Idq_ref.q = Iq_max;
 	}
 	if (Idq_ref.q < -Iq_max) {
 		Idq_ref.q = -Iq_max;
 	}
+
 	Idq_ref.d = 0.0F;
 	Iabc.a = I1_low_value;
 	Iabc.b = I2_low_value;
 	Iabc.c = -(Iabc.a + Iabc.b);
+
 	Idq = Transform::to_dqo(Iabc, angle_4_control);
 	Vdq.d = pi_d.calculateWithReturn(Idq_ref.d, Idq.d);
 	Vdq.q = pi_q.calculateWithReturn(Idq_ref.q, Idq.q);
 	Vdq.o = 0.0F;
+
 	Vabc = Transform::to_threephase(Vdq, angle_4_control);
 }
 
+/**
+ * Helper function that computes duty cycles from ABC frame.
+ */
 inline void compute_duties()
 {
 	inverse_Vhigh = 1.0 / MIN_DC_VOLTAGE;
@@ -330,6 +390,9 @@ inline void compute_duties()
 	duty_abc.c = (Vabc.c * inverse_Vhigh + 0.5);
 }
 
+/**
+ * Helper function that set the duty cycles to the power shield.
+ */
 inline void apply_duties()
 {
 	shield.power.setDutyCycle(LEG1, duty_abc.a);
@@ -337,6 +400,9 @@ inline void apply_duties()
 	shield.power.setDutyCycle(LEG3, duty_abc.c);
 }
 
+/**
+ * Helper function to start the power shield PWMs
+ */
 void start_pwms_ifnot()
 {
 	if (!pwm_enable) {
@@ -345,40 +411,48 @@ void start_pwms_ifnot()
 	}
 }
 
+/**
+ * Setter function for required variables
+ */
 void init_variables()
 {
-	/* to count the time */
+	/* Time counter */
 	counter_time = 0;
-	/* variable for measurements */
+	/* Measurements variables */
 	I1_low_value = 0.0F;
 	I2_low_value = 0.0F;
 	I_high = 0.0F;
 	V_high = 0.0F;
-	/* offset variables */
+	/* Offset variables */
 	I1_offset = 0.0F;
 	I2_offset = 0.0F;
 	tmpI1_offset = 0.0F;
 	tmpI2_offset = 0.0F;
-	/* state view of the pwm */
+	/* State view of the pwm */
 	pwm_enable = false;
-	/* idle or power mode*/
+	/* Idle or power mode*/
 	asked_mode = IDLEMODE;
-	/* we begin to measure the current offset before all */
+	/* We begin to measure the current offset before all */
 	control_state = OFFSET_ST;
-	/* */
 	Iq_max = 2.0;
 	manual_Iq_ref = 0.0F;
 }
-//--------------SETUP FUNCTIONS-------------------------------
+/* --------------SETUP FUNCTIONS------------------------------- */
+
 /**
- * This is the setup routine.
- * It is used to call functions that will initialize your spin, twist, data
- * and/or tasks. In this example, we setup the version of the spin board and a
- * background task. The critical task is defined but not started.
+ * In this setup routine :
+ *  - Power shield is initialized
+ * 		- Shield is set in Buck Mode.
+ * 		- Default sensors are activated
+ * 		- GPIOs are set for Hall effect sensor
+ *  - ScopeMimicry is initialized
+ * 	- VHigh filter and PIDs are initialized
+ * 	- LED is turned on.
+ *  - Tasks are initialized and started
  */
 void setup_routine()
 {
-	// Setup the hardware first
+	/* Setup the hardware first */
 	shield.power.initBuck(ALL);
 	shield.sensors.enableDefaultOwnverterSensors();
 
@@ -386,13 +460,13 @@ void setup_routine()
 	spin.gpio.configurePin(HALL2, INPUT);
 	spin.gpio.configurePin(HALL3, INPUT);
 
-	// scope configuration
+	/* Scope configuration */
 	scope.connectChannel(V12_value, "V12_value");           /* 0 */
 	scope.connectChannel(Vq, "Vq");                         /* 1 */
 	scope.connectChannel(Vd, "Vd");                         /* 2 */
 	scope.connectChannel(I1_low_value, "I1_low_value");     /* 3 */
 	scope.connectChannel(I2_low_value, "I2_low_value");     /* 4 */
-	scope.connectChannel(I_high, "I_high_value");     	  /* 5 */
+	scope.connectChannel(I_high, "I_high_value");     	    /* 5 */
 	scope.connectChannel(Iq_meas, "Iq_meas");               /* 6 */
 	scope.connectChannel(angle_filtered, "angle_filtered"); /* 7 */
 	scope.connectChannel(Ib_ref, "Ib_ref");                 /* 8 */
@@ -402,28 +476,37 @@ void setup_routine()
 	scope.set_trigger(&mytrigger);
 	scope.set_delay(0.0);
 	scope.start();
+
 	/* Initialize values */
 	init_filt_and_reg();
 	init_variables();
 	spin.led.turnOn();
-	// Declare tasks
-	uint32_t background_task_number = task.createBackground(loop_background_task);
+
+	/* Declare tasks */
+	uint32_t background_task_number =
+					task.createBackground(loop_background_task);
+
 	uint32_t app_task_number = task.createBackground(application_task);
 	task.createCritical(loop_critical_task, control_task_period);
-	// Finally, start tasks
+
+	/* Finally, start tasks */
 	task.startBackground(background_task_number);
 	task.startBackground(app_task_number);
 	task.startCritical();
 }
-//--------------LOOP FUNCTIONS--------------------------------
+
+/* --------------LOOP FUNCTIONS-------------------------------- */
+
+/**
+ * This background task retrieve user inputs to control the OwnVerter:
+ * - P and I keys respectively Power ON and Power OFF the inverter
+ * - U and D keys respectively Increase and Decrease Iq reference.
+ * - R Q and M keys are used to control ScopeMimicry data retrieval.
+ */
 void loop_background_task()
 {
-	// Task content
 	received_serial_char = console_getchar();
 	switch (received_serial_char) {
-	case 'h':
-		printk("help");
-		break;
 	case 'p':
 		printk("power asked");
 		asked_mode = POWERMODE;
@@ -442,16 +525,19 @@ void loop_background_task()
 		manual_Iq_ref -= 0.1F;
 		break;
 	case 'm':
-		/* to print scope datas in ownplot as possible */
+		/* To print scope datas in ownplot as soon as possible */
 		memory_print = !memory_print;
 		break;
 	case 'q':
-		/* relaunch scope acquisition */
+		/* Relaunch scope acquisition */
 		scope.start();
 		break;
 	}
 }
 
+/**
+ * This application task sends data over USB Serial.
+ */
 void application_task()
 {
 	if (!memory_print) {
@@ -465,7 +551,7 @@ void application_task()
 		/* If memory_print is true then we plot scope datas in an infinite loop
 		 * This can be used with ownplot if you have not python script installed to
 		 * plot downloaded data using dump_scope_datas().
-		* */
+		 */
 		k_app_idx = (k_app_idx + 1) % SCOPE_SIZE;
 		printk("%.2f:", scope.get_channel_value(k_app_idx, 0));
 		printk("%.2f:", scope.get_channel_value(k_app_idx, 1));
@@ -517,6 +603,11 @@ void application_task()
 	task.suspendBackgroundMs(250);
 }
 
+
+/**
+ * This is the critical task that runs at 10kHz.
+ * It performs the Field Oriented Control.
+ */
 void loop_critical_task()
 {
 	counter_time++;
@@ -538,6 +629,7 @@ void loop_critical_task()
 		stop_pwm_and_reset_states_ifnot();
 		break;
 	case POWER_ST:
+		/* Control loop is executed here */
 		control_torque();
 		compute_duties();
 		apply_duties();
@@ -545,6 +637,7 @@ void loop_critical_task()
 		break;
 	}
 
+	/* Decimation is used to reduce rate of plotting in ScopeMimicry */
 	if (counter_time % decimation == 0) {
 		angle_index_f = angle_index;
 		Va = Vabc.a;
