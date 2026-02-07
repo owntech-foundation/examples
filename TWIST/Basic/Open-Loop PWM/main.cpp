4111b3fe775596bd6d9ae8cd5c78857e67a84b8b
Luiz Villa <luiz.villa@laas.fr>
2026-02-07 01:29:59 +0100

    introduces a main.cpp for the basic open-loop example

---
+++ TWIST/Basic/Open-Loop PWM/main.cpp
@@ -0,0 +1,263 @@
+/*
+ * Copyright (c) 2021-present LAAS-CNRS
+ *
+ *   This program is free software: you can redistribute it and/or modify
+ *   it under the terms of the GNU Lesser General Public License as published by
+ *   the Free Software Foundation, either version 2.1 of the License, or
+ *   (at your option) any later version.
+ *
+ *   This program is distributed in the hope that it will be useful,
+ *   but WITHOUT ANY WARRANTY; without even the implied warranty of
+ *   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ *   GNU Lesser General Public License for more details.
+ *
+ *   You should have received a copy of the GNU Lesser General Public License
+ *   along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ * SPDX-License-Identifier: LGPL-2.1
+ */
+
+/**
+ * @brief  This example demonstrates how to deploy a Buck converter with
+ *         voltage mode control on the Twist power shield.
+ *
+ * @author Clément Foucher <clement.foucher@laas.fr>
+ * @author Luiz Villa <luiz.villa@laas.fr>
+ * @author Ayoub Farah Hassan <ayoub.farah-hassan@laas.fr>
+ */
+
+/*--------------Zephyr---------------------------------------- */
+#include <zephyr/console/console.h>
+
+/*--------------OWNTECH APIs---------------------------------- */
+#include "SpinAPI.h"
+#include "ShieldAPI.h"
+#include "TaskAPI.h"
+
+/*--------------OWNTECH Libraries----------------------------- */
+#include "pid.h"
+
+/*--------------SETUP FUNCTIONS DECLARATION------------------- */
+/* Setups the hardware and software of the system */
+void setup_routine();
+
+/*--------------LOOP FUNCTIONS DECLARATION-------------------- */
+/* Code to be executed in the slow communication task */
+void loop_communication_task();
+/* Code to be executed in the background task */
+void loop_application_task();
+/* Code to be executed in real time in the critical task */
+void loop_critical_task();
+
+/*--------------USER VARIABLES DECLARATIONS------------------- */
+
+/* [us] period of the critical task */
+static uint32_t control_task_period = 100;
+/* [bool] state of the PWM (critical task) */
+static bool pwm_enable = false;
+
+uint8_t received_serial_char;
+
+/* Measure variables */
+
+static float32_t V1_low_value;
+static float32_t V2_low_value;
+static float32_t I1_low_value;
+static float32_t I2_low_value;
+static float32_t I_high;
+static float32_t V_high;
+
+static float32_t temp_1_value;
+static float32_t temp_2_value;
+
+/* Temporary storage fore measured value (critical task) */
+static float meas_data;
+
+float32_t duty_cycle = 0.3;
+
+/* Voltage reference */
+static float32_t voltage_reference = 15;
+
+/*--------------------------------------------------------------- */
+
+/* LIST OF POSSIBLE MODES FOR THE OWNTECH CONVERTER */
+enum serial_interface_menu_mode
+{
+    IDLEMODE = 0,
+    POWERMODE
+};
+
+uint8_t mode = IDLEMODE;
+
+/*--------------SETUP FUNCTIONS------------------------------- */
+
+/**
+ * This is the setup routine.
+ * Here the setup :
+ *  - Initializes the power shield in Buck mode
+ *  - Initializes the power shield sensors
+ *  - Spawns three tasks.
+ */
+void setup_routine()
+{
+    /* Buck voltage mode */
+    shield.power.initBuck(ALL);
+
+    shield.sensors.enableDefaultTwistSensors();
+
+    /* Then declare tasks */
+    uint32_t app_task_number = task.createBackground(loop_application_task);
+    uint32_t com_task_number = task.createBackground(loop_communication_task);
+    task.createCritical(loop_critical_task, 100);
+
+    /* Finally, start tasks */
+    task.startBackground(app_task_number);
+    task.startBackground(com_task_number);
+    task.startCritical();
+}
+
+/*--------------LOOP FUNCTIONS-------------------------------- */
+
+/**
+ * This tasks implements a minimalistic USB serial interface to control
+ * the buck converter.
+ */
+void loop_communication_task()
+{
+    received_serial_char = console_getchar();
+    switch (received_serial_char)
+    {
+    case 'h':
+        /*----------SERIAL INTERFACE MENU----------------------- */
+        printk(" ________________________________________ \n"
+               "|     ---- MENU buck voltage mode ----   |\n"
+               "|     press i : idle mode                |\n"
+               "|     press p : power mode               |\n"
+               "|     press u : duty cycle UP            |\n"
+               "|     press d : duty cycle DOWN          |\n"
+               "|________________________________________|\n\n");
+        /*------------------------------------------------------ */
+        break;
+    case 'i':
+        printk("idle mode\n");
+        mode = IDLEMODE;
+        break;
+    case 'p':
+        printk("power mode\n");
+        mode = POWERMODE;
+        break;
+    case 'u':
+        duty_cycle += 0.01;
+        break;
+    case 'd':
+        duty_cycle -= 0.01;
+        break;
+    default:
+        break;
+    }
+}
+
+/**
+ * This is the code loop of the background task
+ * This task mostly logs back measurements to the USB serial interface.
+ */
+void loop_application_task()
+{
+    switch (mode)
+    {
+    case IDLEMODE:
+        spin.led.turnOff();
+        break;
+    case POWERMODE:
+        spin.led.turnOn();
+
+        shield.sensors.triggerTwistTempMeas(TEMP_SENSOR_1);
+        shield.sensors.triggerTwistTempMeas(TEMP_SENSOR_2);
+
+        meas_data = shield.sensors.getLatestValue(TEMP_SENSOR_1);
+        if (meas_data != NO_VALUE) temp_1_value = meas_data;
+
+        meas_data = shield.sensors.getLatestValue(TEMP_SENSOR_2);
+        if (meas_data != NO_VALUE) temp_2_value = meas_data;
+
+        printk("%.3f:", (double)I1_low_value);
+        printk("%.3f:", (double)V1_low_value);
+        printk("%.3f:", (double)duty_cycle);
+        printk("%.3f:", (double)I2_low_value);
+        printk("%.3f:", (double)V2_low_value);
+        printk("%.3f:", (double)duty_cycle);
+        printk("%.3f:", (double)I_high);
+        printk("%.3f:", (double)V_high);
+        printk("%.3f:", (double)temp_1_value);
+        printk("%.3f:", (double)temp_2_value);
+        printk("\n");
+        break;
+    default:
+        break;
+    }
+    task.suspendBackgroundMs(100);
+}
+
+/**
+ * This is the code loop of the critical task
+ * This task runs at 10kHz.
+ *  - It retrieves sensors values
+ *  - It update the duty cycle of the PWM signals
+ */
+void loop_critical_task()
+{
+    meas_data = shield.sensors.getLatestValue(I1_LOW);
+    if (meas_data != NO_VALUE) I1_low_value = meas_data;
+
+    meas_data = shield.sensors.getLatestValue(V1_LOW);
+    if (meas_data != NO_VALUE) V1_low_value = meas_data;
+
+    meas_data = shield.sensors.getLatestValue(V2_LOW);
+    if (meas_data != NO_VALUE) V2_low_value = meas_data;
+
+    meas_data = shield.sensors.getLatestValue(I2_LOW);
+    if (meas_data != NO_VALUE) I2_low_value = meas_data;
+
+    meas_data = shield.sensors.getLatestValue(I_HIGH);
+    if (meas_data != NO_VALUE) I_high = meas_data;
+
+    meas_data = shield.sensors.getLatestValue(V_HIGH);
+    if (meas_data != NO_VALUE) V_high = meas_data;
+
+
+    switch (mode)
+    {
+    case IDLEMODE:
+        if (pwm_enable == true)
+        {
+            shield.power.stop(ALL);
+            pwm_enable = false;
+        }
+        break;
+
+    case POWERMODE:
+        shield.power.setDutyCycle(ALL, duty_cycle);
+
+        /* Set POWER ON */
+        if (!pwm_enable)
+        {
+            shield.power.start(ALL);
+            pwm_enable = true;
+        }
+        break;
+    default:
+        break;
+    }
+
+}
+
+/**
+ * This is the main function of this example
+ * This function is generic and does not need editing.
+ */
+int main(void)
+{
+    setup_routine();
+
+    return 0;
+}
